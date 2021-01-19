45ab33060dc8429b68eb8a1c359a2425e13bafb6
Oleg <sonziza@yandex.ru>
2021-01-19 22:30:40 +0300

    Добавьте файлы проекта.

---
+++ AspProject/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspProject
+{
+    /// <summary>
+    /// C#9 - используем record дл€ создани€ авто-конструктора
+    /// </summary>
+    public record Startup(IConfiguration Configuration)
+    {
+        /*
+        /// <summary>
+        /// ƒобавл€ем свойство дл€ доступа к конфигурации
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// ƒобавл€ем новый конструктор, принимающий интерфейс Configuration
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        */
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
