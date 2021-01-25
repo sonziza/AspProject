62c21056d19a333f68b267bbdc5b1274078690b6
Oleg <sonziza@yandex.ru>
2021-01-25 21:47:38 +0300

    Создал интерфейс для будущего сервиса работы со списком сотрудников

---
+++ AspProject/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,18 @@
+﻿using AspProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspProject.Infrastructure.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+        Employee Get(int id);
+        int Add(Employee employee);
+        void Update(Employee employee);
+        bool Delete(int id);
+
+    }
+}
