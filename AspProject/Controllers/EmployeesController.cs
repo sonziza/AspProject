e98b52a1647bfe9468939f62209295dbf729d950
Oleg <sonziza@yandex.ru>
2021-01-26 14:52:18 +0300

    Добавлен функционал Edit в контроллер Employee и во View

--- AspProject/Controllers/EmployeesController.cs
+++ AspProject/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using AspProject.Data;
 using AspProject.Infrastructure.Interfaces;
 using AspProject.Models;
+using AspProject.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -23,11 +24,46 @@ namespace AspProject.Controllers
         public IActionResult Details(int id)
         {
             var employee = _EmployeesData.Get(id);
-                if (employee is not null)
-                {
-                    return View(employee);
-                }
+            if (employee is not null)
+            {
+                return View(employee);
+            }
             return NotFound();
         }
+        #region Edit
+        public IActionResult Edit(int id)
+        {
+            if (id <= 0) return BadRequest();
+            var employee = _EmployeesData.Get(id);
+            if (employee is null)
+                return NotFound();
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+                Post = employee.Post
+            });
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            if (model is null) throw new ArgumentNullException(nameof(model));
+
+            var employee = new Employee
+            {
+                Id = model.Id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Patronymic = model.Patronymic,
+                Age = model.Age,
+                Post = model.Post
+            };
+            _EmployeesData.Update(employee);
+            return RedirectToAction("Index");
+        }
+        #endregion
     }
 }
