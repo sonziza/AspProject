28f188d2e99bdb403d2cc070929a1321133fba67
Oleg <sonziza@yandex.ru>
2021-01-29 12:57:11 +0300

    Доменная модель проекта; -создание базовых сущностей -создание сущностей brand и section

---
+++ AspProjectDomain/Entities/Base/Interfaces/IOrderedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspProjectDomain.Entities.Base.Interfaces
+{
+    public interface IOrderedEntity : INamedEntity
+    {
+        int Order { get; set; }
+    }
+}
---
+++ AspProjectDomain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,13 @@
+﻿using AspProjectDomain.Entities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspProjectDomain.Entities.Base
+{
+    public class NamedEntity : INamedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
