f3e0ddae86744dc0e65834812a179ed67024f766
Oleg <sonziza@yandex.ru>
2021-02-04 15:53:55 +0300

    Настраиваем сущности для будущей БД

--- AspProjectDomain/Entities/Product.cs
+++ AspProjectDomain/Entities/Product.cs
@@ -2,6 +2,7 @@
 using AspProjectDomain.Entities.Base.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,19 @@ namespace AspProjectDomain.Entities
 
         public int SectionId { get; set; }
 
+        //отношение "один-ко-многим"
+        [ForeignKey(nameof(SectionId))]
+        public Section Section { get; set; }
+
+        //т.к. у нас в ходе проектирования уже используется св-во 
+        //BrandId и тп, то используем два свойства
         public int? BrandId { get; set; }
+        [ForeignKey(nameof(BrandId))]
+        public Brand Brand { get; set; }
 
         public string ImageUrl { get; set; }
-
+        //!!!для БД важно указать точность!!!
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
