5a4c25e92755f5ccb37c32413fb19355d8664d09
Oleg <sonziza@yandex.ru>
2021-02-16 15:47:03 +0300

    подключаем сервис Identity в startup.cs

--- AspProject/Startup.cs
+++ AspProject/Startup.cs
@@ -10,6 +10,9 @@ using AspProject.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using AspProject.Data;
 using AspProject.Infrastructure.Services.InSQL;
+using AspProjectDomain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace AspProject
 {
@@ -23,6 +26,47 @@ namespace AspProject
         {
             services.AddDbContext<AspProjectDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<AspProjectDBInitializer>();
+
+            //если юзер и роль по умолчанию
+            //services.AddIdentity<IdentityUser, IdentityRole>();
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<AspProjectDbContext>()
+                .AddDefaultTokenProviders();
+
+            //конфигурация системы Identity
+            // - требование к паролю
+            services.Configure<IdentityOptions>(opt =>
+            {
+#if DEBUG
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+                opt.User.RequireUniqueEmail = false; //будет ли использоваться почта логином
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 10;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+            });
+
+            //конфигурация системы Cookies
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Cookie.Name = "AspProject.ru";
+                opt.Cookie.HttpOnly = true; //не передавать в куки ftp tcp и проч протоколы
+                opt.ExpireTimeSpan = TimeSpan.FromDays(10); //через 10 дней куки "протухнут"
+
+                opt.LoginPath = "/Account/Login";
+                opt.LogoutPath = "/Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                //этот параметро заставляет сделать новый id сеанса для разделения пользователей анонимов и залогиненых
+                opt.SlidingExpiration = true;
+            });
 
             services.AddTransient<IEmployeesData, EmployeesDataInMemory>();
             //services.AddTransient<IProductData, InMemoryProductData>();
@@ -45,16 +89,20 @@ namespace AspProject
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();    //включаем отладочную страницу в браузере
             }
 
             //для подтягивания файлов на хосте (css файлы, картинки и проч.)
             app.UseStaticFiles();
 
+            //система маршрутизации
             app.UseRouting();
 
-
-            var greetings = Configuration["Greetings"];
+            //требования аутен-ции и автор-ции
+            //извлекает инфо о пользователе
+            app.UseAuthentication();
+            //проверяется доступ по маршруту
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
