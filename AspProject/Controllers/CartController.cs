3446daff97194f70567cf680e8c6836e9b6f7c3b
Oleg <sonziza@yandex.ru>
2021-03-03 17:13:20 +0300

    Корзина - контроллер

---
+++ AspProject/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using AspProject.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspProject.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService) => _CartService = CartService;
+
+        public IActionResult Index() => View(_CartService.GetViewModel());
+
+        public IActionResult Add(int id)
+        {
+            _CartService.Add(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int id)
+        {
+            _CartService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Decrement(int id)
+        {
+            _CartService.Decrement(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Clear()
+        {
+            _CartService.Clear();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
