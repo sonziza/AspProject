f5b415ba5d51af012bbc3e35b31b1766eae0f6b3
Oleg <sonziza@yandex.ru>
2021-03-11 16:33:32 +0300

    Сервис заказов - создаем интерфейс сервиса + ViewModel

---
+++ AspProject/ViewModel/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AspProject.ViewModel
+{
+    public class OrderViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
