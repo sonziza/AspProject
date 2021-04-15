50f93a66966fffdff5df980da842396a1e0c0486
Oleg <sonziza@yandex.ru>
2021-04-15 23:23:22 +0300

    Новый проект AspProject.Interfaces - перенос Интерфейсов

---
+++ Services/AspProject.Interfaces/Services/IProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspProjectDomain;
+using AspProjectDomain.Entities;
+
+namespace AspProject.Infrastructure.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
+        Product GetProductById(int id);
+    }
+}
