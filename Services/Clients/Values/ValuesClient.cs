ba9adf9a2dd60f10843e6957c07ca3daa21e47aa
Oleg <sonziza@yandex.ru>
2021-04-17 17:00:29 +0300

    реализация клиента WewbApi сервиса

---
+++ Services/Clients/Values/ValuesClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Clients.Base;
+using Microsoft.Extensions.Configuration;
+
+namespace Clients.Values
+{
+    class ValuesClient:BaseClient
+    {
+        public ValuesClient(IConfiguration Configuration) : base(Configuration, "api/values") { }
+
+
+        public IEnumerable<string> Get()
+        {
+            //запрос
+            var response = Http.GetAsync(Address).Result;
+            //десериализация полученного значения в перечисление
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<IEnumerable<string>>().Result;
+
+            return Enumerable.Empty<string>();
+        }
+
+        public string Get(int id)
+        {
+            var response = Http.GetAsync($"{Address}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<string>().Result;
+
+            return string.Empty;
+        }
+
+        public Uri Create(string value)
+        {
+            var response = Http.PostAsJsonAsync(Address, value).Result;
+            return response.EnsureSuccessStatusCode().Headers.Location;
+        }
+
+        public HttpStatusCode Edit(int id, string value)
+        {
+            var response = Http.PutAsJsonAsync($"{Address}/{id}", value).Result;
+            return response.EnsureSuccessStatusCode().StatusCode;
+        }
+
+        public bool Remove(int id)
+        {
+            var response = Http.DeleteAsync($"{Address}/{id}").Result;
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
