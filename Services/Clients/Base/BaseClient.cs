6bba3d52b247ec8b4704b7696630d6081c43cb8c
Oleg <sonziza@yandex.ru>
2021-04-19 17:03:26 +0300

    Доработка базового клиента

--- Services/Clients/Base/BaseClient.cs
+++ Services/Clients/Base/BaseClient.cs
@@ -31,5 +31,56 @@ namespace Clients.Base
                 }
             };
         }
+
+        protected T Get<T>(string url) => GetAsync<T>(url).Result; //.GetAwaiter().GetResult();
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await Http.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await Http.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await Http.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await Http.DeleteAsync(url);
+            return response;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            //GC.SuppressFinalize(this); // при наличии  ~BaseClient() => Dispose(false);
+        }
+
+        //~BaseClient() => Dispose(false);
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_Disposed) return;
+            if (disposing)
+            {
+                // Очистка управляемых ресурсов
+                Http.Dispose();
+            }
+
+            // Очистка неуправляемых ресурсов
+
+            _Disposed = true;
+        }
     }
 }
