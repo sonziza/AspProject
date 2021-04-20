bc402f7dd3460ec08df389930a249cd1d5d39d9a
Oleg <sonziza@yandex.ru>
2021-04-20 15:51:27 +0300

    контроллер ProductsAPI

--- Services/AspProject.Interfaces/WebAPI.cs
+++ Services/AspProject.Interfaces/WebAPI.cs
@@ -10,5 +10,6 @@ namespace AspProject.Interfaces
     public static class WebAPI
     {
         public const string Employees = "api/employees";
+        public const string Products = "api/products";
     }
 }
