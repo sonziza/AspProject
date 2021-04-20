5462021a586568594c2da49dfc4679bc82584af4
Oleg <sonziza@yandex.ru>
2021-04-20 17:08:01 +0300

    Создаем клиента заказов

---
+++ Services/Clients/Orders/OrdersClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AspProject.Interfaces;
+using AspProject.Interfaces.Services;
+using AspProjectDomain.DTO;
+using Clients.Base;
+using Microsoft.Extensions.Configuration;
+
+namespace Clients.Orders
+{
+    public class OrdersClient:BaseClient, IOrderService
+    {
+        public OrdersClient(IConfiguration Configuration) : base(Configuration, WebAPI.Orders) { }
+
+        public async Task<IEnumerable<OrderDTO>> GetUserOrders(string UserName) =>
+            await GetAsync<IEnumerable<OrderDTO>>($"{Address}/user/{UserName}");
+
+        public async Task<OrderDTO> GetOrderById(int id) => await GetAsync<OrderDTO>($"{Address}/{id}");
+
+        public async Task<OrderDTO> CreateOrder(string UserName, CreateOrderModel OrderModel)
+        {
+            var response = await PostAsync($"{Address}/{UserName}", OrderModel);
+            return await response.Content.ReadAsAsync<OrderDTO>();
+        }
+    }
+}
